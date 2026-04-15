0226f9451361a0353db46c6896700265a7f6f176
J2queue <sportyjoce@gmail.com>
2026-04-15 03:13:40 -0400

    merge into main

--- src/pages/Auth/login/login.jsx
+++ src/pages/Auth/login/login.jsx
@@ -60,7 +60,7 @@ export default function LoginForm({ setAuthView, setLoggedIn, setUserOrg }) {
 
     // Your original fetch logic
     try {
-      const res = await fetch("http://localhost:5000/login", {
+      const res = await fetch("https://formbuilderbackend-d26n.onrender.com/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         credentials: "include", // important for cookies
--- src/pages/Auth/register/register.jsx
+++ src/pages/Auth/register/register.jsx
@@ -72,7 +72,7 @@ export default function RegisterForm({ setAuthView }) {
     }
 
     try {
-      const res = await fetch("http://127.0.0.1:5000/register", {
+      const res = await fetch("https://formbuilderbackend-d26n.onrender.com/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
