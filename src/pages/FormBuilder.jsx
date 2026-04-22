a8e95311c9f1ab6f3378caef54115398b2bd2bf2
J2queue <sportyjoce@gmail.com>
2026-04-22 14:14:45 -0400

    Fix Multiple Choice and Dropdown question types

--- src/pages/FormBuilder.jsx
+++ src/pages/FormBuilder.jsx
@@ -190,8 +190,8 @@ function FormBuilder({ setLoggedIn, organization }) {
                   <MenuItem value="number">Number</MenuItem>
                   <MenuItem value="date">Date</MenuItem>
                   <MenuItem value="textarea">Long Text</MenuItem>
-                 {/* <MenuItem value="select">Dropdown</MenuItem>
-                  <MenuItem value="radio">Multiple Choice</MenuItem> couldnt get it working :/*/} 
+                  <MenuItem value="select">Dropdown</MenuItem>
+                  <MenuItem value="radio">Multiple Choice</MenuItem>
                 </TextField>
                 
                 <FormControlLabel 
